87908bfafb816f8fb0296754308e2d9cc88f36b4
byronhormillosa <byronhormillosa@gmail.com>
2018-06-11 01:00:08 -0500

    checkpoint 12 something album will go here

--- bloc-jams-react/src/components/Album.js
+++ bloc-jams-react/src/components/Album.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Album = () => (
   <section className="Album">
-    Album page will go here
+    {this.props.match.params.slug} Album will go here 
   </section>
 );
 export default Album;
